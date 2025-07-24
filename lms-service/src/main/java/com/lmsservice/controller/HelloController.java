d975d1e5ffc9c01fdeae9d003b47c374808d1d0c
Sondev1004 <son.lyhoang2014@gmail.com>
2025-07-24 22:02:56 +0700

    add HelloController with Swagger documentation and update configuration files

--- lms-service/src/main/java/com/lmsservice/controller/HelloController.java
+++ lms-service/src/main/java/com/lmsservice/controller/HelloController.java
@@ -1,10 +1,16 @@
 package com.lmsservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
+@RequestMapping("/test")
 public class HelloController {
+
+    @Operation(summary = "Test API", description = "Mô tả chi tiết về API này")
     @GetMapping("/hello")
     public String hello() {
         return "Hello, LMS Service!";
