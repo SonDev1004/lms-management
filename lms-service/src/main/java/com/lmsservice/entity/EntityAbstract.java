b3436021012261989ccaaf6900bc5653a0bbf8fb
DT262322 <phong.van@itbeesolutions.com>
2025-07-28 17:36:47 +0700

    Enhance EntityAbstract with Lombok annotations for getter/setter and access level

--- lms-service/src/main/java/com/lmsservice/entity/EntityAbstract.java
+++ lms-service/src/main/java/com/lmsservice/entity/EntityAbstract.java
@@ -1,8 +1,15 @@
 package com.lmsservice.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 @MappedSuperclass
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PROTECTED)
 public abstract class EntityAbstract {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
