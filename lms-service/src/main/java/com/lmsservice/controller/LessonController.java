b7ae3e081a91d28e8fa5b732caf2f6b5bd227745
LTN29 <nhanle29102001@gmail.com>
2025-08-10 15:25:06 +0700

    feat: implement lesson creation functionality with validation

--- lms-service/src/main/java/com/lmsservice/controller/LessonController.java
+++ lms-service/src/main/java/com/lmsservice/controller/LessonController.java
@@ -1,5 +1,6 @@
 package com.lmsservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -24,6 +25,7 @@ import lombok.experimental.FieldDefaults;
 public class LessonController {
     final LessonService lessonService;
 
+    @Operation(summary = "TẠO MỚI LESSON", description = "API TẠO MỚI LESSON")
     @PreAuthorize("hasAnyRole('TEACHER','ACADEMIC_MANAGER')")
     @PostMapping("/create")
     public ApiResponse<LessonResponse> createLesson(@RequestBody @Valid LessonRequest request) {
