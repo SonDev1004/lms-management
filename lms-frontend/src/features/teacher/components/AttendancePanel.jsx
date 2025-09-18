32a965031462e9248201a11e174839d3977eeb0d
Dương Hữu Phước <huuphuoc29791@gmail.com>
2025-09-18 18:55:15 +0700

    Attendance

--- lms-frontend/src/features/teacher/components/AttendancePanel.jsx
+++ lms-frontend/src/features/teacher/components/AttendancePanel.jsx
@@ -1,128 +1,139 @@
 //use and React:
-import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { Card, Button } from "primereact";
+import { useEffect, useRef, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Card, Button } from 'primereact';
+import { Calendar } from 'primereact/calendar';
+import { InputText } from 'primereact/inputtext';
 
 //component
-import SessionSelect from "../../session/components/SessionSelect.jsx";
-import AttendanceTable from "./AttendanceTable";
+import SessionSelect from '../../session/components/SessionSelect.jsx';
+import AttendanceTable from './AttendanceTable';
 
 //Cấu hình 3 loại điểm danh cho 3 nút Radio Button
 const attendance_types = [
-    { label: "Vắng", value: "Absent" },
-    { label: "Đi trễ", value: "Late" },
-    { label: "Có mặt", value: "Present" }
+	{ label: 'Có mặt', value: 1 },
+	{ label: 'Đi trễ', value: 2 },
+	{ label: 'Vắng', value: 0 }
 ];
 //mock
-import { attendanceData } from '../../../mocks/mockAttendance';
+import {
+	attendanceData,
+	singleAttendanceData,
+	singleEmptyAttendanceData
+} from '../../../mocks/mockAttendance';
 
 const AttendancePanel = () => {
-    const navigate = useNavigate();
-    // const { courseId } = useParams();
-    const today = new Date().toISOString().slice(0, 10);
+	const navigate = useNavigate();
+	// Ngày đang chọn trong Calendar
+	const [selectedDate, setSelectedDate] = useState(new Date());
+	// Ca học tương ứng của ngày đó
+	const [selectedShift, setSelectedShift] = useState('');
 
-    //Lọc ra session hnay:
-    const sessionsToday = attendanceData.sessions.filter(session => session.date === today);
-    const [sessionId, setSessionId] = useState(sessionsToday.length === 1 ? sessionsToday[0]?.id : null);
+	// State cho bảng điểm danh
+	const [attendanceData, setAttendanceData] = useState([]);
+	useEffect(() => {
+		// Mock-up data. Đúng ra là call API để lấy bảng điểm danh của ngày selectedDate
+		// Mock-up demo: Nếu là ngày trong quá khứ thì đã có dữ liệu điểm danh, ngược lại thì chưa có
+		let today = new Date();
+		if (selectedDate < today) {
+			setAttendanceData(singleAttendanceData);
+		} else {
+			setAttendanceData(singleEmptyAttendanceData);
+		}
+	}, [selectedDate]);
 
-    // State cho bảng điễm danh
-    const [attendanceList, setAttendanceList] = useState([]);
-    useEffect(() => {
-        if (!sessionId) return setAttendanceList([]);
-        const updated = attendanceData.students.map(student => {
-            const attendance = student.attendancelist.find(a => a.sessionId === sessionId);
-            return {
-                ...student,
-                attendance: attendance?.status || "none",
-                reasonAbsent: attendance?.reason || ""
-            };
-        })
-        setAttendanceList(updated);
-    }, [sessionId]);
-    //Handle
-    //Cập nhật trạng thái từng học viên trong state
-    const handleAttendanceChange = (studentId, value) => {
-        setAttendanceList(prev =>
-            prev.map(stu =>
-                stu.id === studentId ? { ...stu, attendance: value } : stu));
-    };
-    //Hàm lưu lí do
-    const handleReasonChange = (value, rowIndex) => {
-        setAttendanceList(prev => {
-            const next = [...prev];
-            next[rowIndex] = { ...next[rowIndex], reasonAbsent: value };
-            return next;
-        });
-    };
-    //Hàm lưu điểm danh
-    const handleSaveAttendance = () => {
-        console.log("Đã lưu:", attendanceList);
-        localStorage.setItem('attendanceList', JSON.stringify(attendanceList));
-        alert('Lưu thành công');
-    }
-    //Hàm Refresh lại trang
-    const handleRefresh = () => {
-        if (!sessionId) return setAttendanceList([]);
-        const updated = attendanceData.students.map(student => {
-            const attendance = student.attendancelist.find(a => a.sessionId === sessionId);
-            return {
-                ...student,
-                attendance: attendance?.status || "none",
-                reasonAbsent: attendance?.reason || ""
-            };
-        });
-        setAttendanceList(updated);
-    };
+	// Hàm cập nhật trạng thái điểm danh của học viên
+	const handleAttendanceChange = (studentId, value) => {
+		setAttendanceData(prev =>
+			prev.map(item =>
+				item.id == studentId ? { ...item, attendance: value } : item
+			)
+		);
+	};
 
-    //Header của Card
-    const header = () => (
-        <div className="flex align-items-center justify-content-between">
-            <span className="text-2xl font-bold">Bảng điểm danh</span>
-            <div className="flex gap-2">
-                <Button label="Bảng tổng hợp" className="p-button-outlined p-button-info" />
-                <Button label="QR Code" className="p-button-outlined p-button-success" />
-            </div>
-        </div>
-    );
+	// Hàm lưu điểm danh lên server
+	const handleSaveAttendance = () => {
+		// Mock-up demo: Đúng ra phải submit lên server
+		console.log(attendanceData);
+	};
 
-    //renderFooter hiển thi tổng số lượng chuyên cần:
-    const renderFooter = (typeValue) => {
-        const total = attendanceList.filter(stu => stu.attendance === typeValue).length;
-        return <span>{total}</span>;
-    }
+	// Header của Card
+	const header = () => (
+		<div className='flex align-items-center justify-content-between'>
+			<span className='text-2xl font-bold'>Bảng điểm danh</span>
+			<div className='flex gap-2'>
+				<Button
+					label='Bảng tổng hợp'
+					className='p-button-outlined p-button-info'
+					onClick={() => navigate('full')}
+				/>
+				<Button
+					label='QR Code'
+					className='p-button-outlined p-button-success'
+				/>
+			</div>
+		</div>
+	);
 
-    return (<>
-        <div className="grid mt-2">
-            <div className="col-9">
-                <Card title={header()} >
-                    <SessionSelect
-                        sessions={sessionsToday}
-                        sessionId={sessionId}
-                        onChange={setSessionId}
-                    />
-                    {sessionsToday.length === 0 && (
-                        <div className="text-red-500 my-3">Không có ca học nào hôm nay.</div>
-                    )}
-                    <AttendanceTable
-                        attendanceList={attendanceList}
-                        attendance_types={attendance_types}
-                        handleAttendanceChange={handleAttendanceChange}
-                        handleReasonChange={handleReasonChange}
-                        renderFooter={renderFooter}
-                    />
-                    <div className="flex justify-content-between flex-wrap mt-4">
-                        <div className="flex align-items-center justify-content-center">
-                            <Button label="Quay lại" onClick={() => navigate(-1)} />
-                            <Button label="Refresh" className="ml-2" onClick={() => handleRefresh()} />
-                        </div>
-                        <div className="flex align-items-center justify-content-center">
-                            <Button label="Lưu" onClick={() => handleSaveAttendance()} />
-                        </div>
-                    </div>
-                </Card>
-            </div>
-        </div>
-    </>);
-}
+	//renderFooter hiển thị tổng số lượng chuyên cần:
+	const renderFooter = typeValue => {
+		const total = 0;
+		return <span>{total}</span>;
+	};
+
+	return (
+		<>
+			<div className='grid mt-2'>
+				<div className='col-9'>
+					<Card title={header()}>
+						<div className='formgrid grid'>
+							<div className='field col'>
+								<label htmlFor='session'>Buổi học</label>
+								<Calendar
+									id='session'
+									value={selectedDate}
+									dateFormat='dd/mm/yy'
+									showIcon
+									showButtonBar
+									onChange={e => setSelectedDate(e.value)}
+								/>
+							</div>
+							<div className='field col'>
+								<label htmlFor='session'>Ca học</label>
+								<InputText value={selectedShift} readOnly />
+							</div>
+						</div>
+						<AttendanceTable
+							attendanceList={attendanceData}
+							attendance_types={attendance_types}
+							handleAttendanceChange={handleAttendanceChange}
+							// handleReasonChange={handleReasonChange}
+							renderFooter={renderFooter}
+						/>
+						<div className='flex justify-content-between flex-wrap mt-4'>
+							<div className='flex align-items-center justify-content-center'>
+								<Button
+									label='Quay lại'
+									onClick={() => navigate(-1)}
+								/>
+								<Button
+									label='Refresh'
+									className='ml-2'
+									// onClick={handleRefresh}
+								/>
+							</div>
+							<div className='flex align-items-center justify-content-center'>
+								<Button
+									label='Lưu'
+									onClick={() => handleSaveAttendance()}
+								/>
+							</div>
+						</div>
+					</Card>
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default AttendancePanel;
--- lms-frontend/src/features/teacher/components/AttendanceTable.jsx
+++ lms-frontend/src/features/teacher/components/AttendanceTable.jsx
@@ -1,43 +1,78 @@
-import { DataTable } from "primereact/datatable";
-import { Column } from "primereact/column";
-import { RadioButton } from "primereact/radiobutton";
-import { InputText } from "primereact/inputtext";
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { RadioButton } from 'primereact/radiobutton';
+import { InputText } from 'primereact/inputtext';
 
-const AttendanceTable = ({ attendanceList, attendance_types, handleAttendanceChange, handleReasonChange, renderFooter }) => {
-    return (
-        <DataTable showGridlines stripedRows value={attendanceList} tableStyle={{ minWidth: '25rem' }} paginator rows={10}>
-            <Column header="STT" body={(rowData, options) => options.rowIndex + 1} style={{ width: '2.5%' }}></Column>
-            <Column field="code" header="Mã học viên" headerStyle={{ whiteSpace: 'nowrap', textAlign: 'center' }} style={{ width: '10%' }}></Column>
-            <Column field="name" header="Tên học viên" body={rowData => `${rowData.firstname} ${rowData.lastname}`} footer="Tổng" style={{ width: '20%' }} />
-            {attendance_types.map(type => (
-                <Column
-                    key={type.value}
-                    header={type.label}
-                    headerStyle={{ whiteSpace: 'nowrap', textAlign: 'center' }}
-                    bodyStyle={{ textAlign: 'center', verticalAlign: 'middle' }}
-                    body={rowData => (
-                        <RadioButton
-                            inputId={`${type.value}-${rowData.id}`}
-                            name={`attendance-${rowData.id}`}
-                            value={type.value}
-                            onChange={() => handleAttendanceChange(rowData.id, type.value)}
-                            checked={rowData.attendance === type.value}
-                        />
-                    )}
-                    footer={renderFooter(type.value)}
-                />
-            ))}
-            <Column
-                field="reasonAbsent"
-                header="Lý do vắng"
-                body={(rowData, options) =>
-                (<InputText value={rowData.reasonAbsent || ""}
-                    onChange={(e) => handleReasonChange(e.target.value, options.rowIndex)}
-                    placeholder="Lý do vắng"
-                    disabled={rowData.attendance !== "Absent"}
-                />)}
-            />
-        </DataTable>);
-}
+const AttendanceTable = ({
+	attendanceList,
+	attendance_types,
+	handleAttendanceChange,
+	handleReasonChange,
+	renderFooter
+}) => {
+	return (
+		<DataTable
+			showGridlines
+			stripedRows
+			value={attendanceList}
+			tableStyle={{ minWidth: '25rem' }}
+			paginator
+			rows={10}
+		>
+			<Column
+				header='STT'
+				body={(rowData, options) => options.rowIndex + 1}
+				style={{ width: '2.5%' }}
+			></Column>
+			<Column
+				field='code'
+				header='Mã học viên'
+				headerStyle={{ whiteSpace: 'nowrap', textAlign: 'center' }}
+				style={{ width: '10%' }}
+			></Column>
+			<Column
+				field='name'
+				header='Tên học viên'
+				body={rowData => `${rowData.firstname} ${rowData.lastname}`}
+				footer='Tổng'
+				style={{ width: '20%' }}
+			/>
+			{attendance_types.map(type => (
+				<Column
+					key={type.value}
+					header={type.label}
+					headerStyle={{ whiteSpace: 'nowrap', textAlign: 'center' }}
+					bodyStyle={{ textAlign: 'center', verticalAlign: 'middle' }}
+					body={rowData => (
+						<RadioButton
+							inputId={`${type.value}-${rowData.id}`}
+							name={`attendance-${rowData.id}`}
+							value={type.value}
+							onChange={() =>
+								handleAttendanceChange(rowData.id, type.value)
+							}
+							checked={rowData.attendance == type.value}
+						/>
+					)}
+					footer={renderFooter(type.value)}
+				/>
+			))}
+			<Column
+				field='reasonAbsent'
+				header='Lý do vắng'
+				body={(rowData, options) => (
+					<InputText
+						value={rowData.reasonAbsent || ''}
+						onChange={e =>
+							handleReasonChange(e.target.value, options.rowIndex)
+						}
+						placeholder='Lý do vắng'
+						disabled={rowData.attendance !== 'Absent'}
+					/>
+				)}
+			/>
+		</DataTable>
+	);
+};
 
 export default AttendanceTable;
