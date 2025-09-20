b62906e1e908c5e56efe0ae610d4bdc6f4a1982e
leezen2903 <trungnghia.act@gmail.com>
2025-09-20 17:10:29 +0700

    Update CourseDetailTeacher.jsx

--- lms-frontend/src/features/course/pages/CourseDetailTeacher.jsx
+++ lms-frontend/src/features/course/pages/CourseDetailTeacher.jsx
@@ -1,4 +1,5 @@
 import { Button } from "primereact/button";
+import { Card } from "primereact/card";
 import { Outlet, useNavigate } from "react-router-dom";
 
 const CourseDetailTeacher = () => {
@@ -8,6 +9,14 @@ const CourseDetailTeacher = () => {
             <Button label="Danh sách học viên" onClick={() => navigate('student-list')} className="mr-2" />
             <Button label="Bảng điểm danh" onClick={() => navigate('attendance')} />
             <Outlet />
+            <Card
+                className="flex justify-content-end flex-wrap"
+            >
+                <div class="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">1</div>
+                <div class="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">2</div>
+                <div class="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">3</div>
+            </Card>
+
         </div>
         <div>
 
